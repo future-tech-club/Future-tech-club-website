73bfeaa5bb3e5d69e1a100b5651a23c9f24cc6be
future-tech-club <future_tech_club@inptic.edu.dz>
2025-02-22 11:13:10 +0100

    update components

--- src/components/contactus/ContactUs.jsx
+++ src/components/contactus/ContactUs.jsx
@@ -19,7 +19,7 @@ const ContactUs = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:8000/send-email/", {
+      const response = await fetch("https://ftc-website-backend-production.up.railway.app/send-email/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
-  assetsInclude: ['**/*.ttf', '**/*.woff', '**/*.woff2', '**/*.eot', '**/*.svg']
-})
-
+  base: '/ftc-website-frontend/',  
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,
+  },
+});
